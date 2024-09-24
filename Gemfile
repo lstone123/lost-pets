5b929819af5fdfed36b39d1dceea2acfa85c734d
Louise Stone <louisemstone@hotmail.com>
2024-09-24 20:29:21 +0100

    demo complete

--- Gemfile
+++ Gemfile
@@ -64,3 +64,6 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+
+gem "simple_form", github: "heartcombo/simple_form"
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # read (all)
+  get 'pets', to: 'pets#index'
+
+  # create
+  get 'pets/new', to: 'pets#new', as: :new_pet
+  post 'pets', to: 'pets#create'
+
+  # read (one)
+  get 'pets/:id', to: 'pets#show', as: :pet
+
+  # update
+  get 'pets/:id/edit', to: 'pets#edit', as: :edit_pet
+  patch 'pets/:id', to: 'pets#update'
+
+  # delete
+  delete 'pets/:id', to: 'pets#destroy'
 end
